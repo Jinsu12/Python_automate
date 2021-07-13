e1643fc423117a644f55584a7b4e4c7d86e74e13
jinsu12 <jss910924@gmail.com>
2021-07-14 08:51:27 +0900

    210714, day 11 강의

---
+++ blackjack.py
@@ -0,0 +1,27 @@
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+# 카드를 컴퓨터와 내가 2장씩 받는다
+# 카드를 추가로 받을지 말지 결정한다
+## if no = 비교 후 승자 선언
+## if yes = 카드 받고 21넘어가는지 확인
+### 21 넘어가면 패배, 안넘어가면 추가로 카드를 받을것인지 다시 확인
+#### no 선택시 비교 후 승자 선언
+
+
