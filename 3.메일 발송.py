4b3237f7566aaf4da7e01d3786b70ebc4f6fa3e5
Jinsu12 <87022312+Jinsu12@users.noreply.github.com>
2021-07-14 10:33:51 +0900

    Update 3.메일 발송.py
    다른곳에서도 사용하기 쉽게 모든 좌표 정리

--- 3.메일 발송.py
+++ 3.메일 발송.py
@@ -3,11 +3,32 @@ import time
 
 company = int(input("국가순번 입력해주세요 >> "))
 
+'''새편지 클릭 위치'''
 new_mail = 680,780 # 새편지 위치
+
+'''기존 메일 빈공간 클릭 (좌측모니터에 절반 사이즈로 우측에)'''
 empty_space_tocopy = 1750,450 #메일 내용을 복사하기 위해 메일 빈공간 클릭 위치
+title = 1183,336 #제목 복사
+#수신인 복붙하기 위함
+old_mail =1200,220
+
+
+'''엑셀파일 위치 (최대화로 오른쪽 모니터에)'''
 excel_position = 2030,250 # 엑셀파일 위치
+
+
+'''신규 메일 (1/4 사이즈로 왼쪽 모니터 좌측 하단에)'''
+sending_mail = 230,1010 # 보내는 메일의 내용 클릭 좌표
+new_title = 300,850 #제목 붙여넣기
+receiver = 300, 740
+
+
+
+'''파일 드롭위치'''
 pick_point = 950, 250 # 첨부파일 픽업 위치
 drop_point = 700, 900 # 첨부파일 드롭 위치
+
+
 
 def starting_point():
     '''엑셀 시작 위치로 이동 셀 A1로'''
@@ -66,13 +87,12 @@ click_newmail() # 새편지 클릭
 pyautogui.moveTo(empty_space_tocopy) # 메일 내용을 복사하기 위해서 빈 공간 클릭
 time.sleep(0.5)
 pyautogui.moveTo(empty_space_tocopy) # 메일 내용을 복사하기 위해서 빈 공간 클릭
-pyautogui.moveTo(1200,350,0.5)
-pyautogui.click(x=1200,y=330,button='left')
+pyautogui.click(empty_space_tocopy)
+time.sleep(0.3)
+pyautogui.click(title)
 select_and_copy()
-pyautogui.moveTo(300,850)
-pyautogui.click(x=300,y=850,button='left')
+pyautogui.click(new_title)
 paste()
-
 
 
 
@@ -84,18 +104,16 @@ select_company()
 pyautogui.press('right')
 pyautogui.keyUp('ctrl')
 copy()
-pyautogui.moveTo(300,650)#붙여넣기
-pyautogui.click(x=300,y=740,button='left')
+pyautogui.click(receiver)
 paste()
 pyautogui.press('enter')
 
 #메일 본문 내용 복사하기
-pyautogui.moveTo(1750,450,1)
-pyautogui.click(x=1750,y=450,button='left')
+time.sleep(1)
+pyautogui.click(empty_space_tocopy)
 copy()
 pyautogui.keyDown('ctrl')
-pyautogui.moveTo(230,1010)
-pyautogui.click(x=230,y=1010,button='left')
+pyautogui.click(sending_mail)
 pyautogui.press('pageup')
 paste()
 pyautogui.press('enter')
@@ -112,8 +130,7 @@ pyautogui.keyUp('ctrl')
 copy()
 
 # Dear 붙여넣기
-pyautogui.moveTo(1200,220)
-pyautogui.click(x=1200,y=220,button='left')
+pyautogui.click(old_mail)
 pyautogui.keyDown('ctrl')
 pyautogui.press('a')
 pyautogui.keyUp('ctrl')
@@ -121,8 +138,7 @@ paste()
 select_and_copy()
 
 # 메일 내용으로 이동
-pyautogui.moveTo(230,1010)
-pyautogui.click(x=230,y=1010,button='left')
+pyautogui.click(sending_mail)
 pyautogui.press('pageup')
 pyautogui.press('home')
 for i in range(4):
