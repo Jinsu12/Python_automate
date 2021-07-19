e91c87dd5a46ef49dbb6c8a25356ec9ee90b5a67
jinsu12 <jss910924@gmail.com>
2021-07-20 06:27:07 +0900

    DAY 16 (210720)

---
+++ practice.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+bugi = Turtle() # obect = class
+
+print(bugi)
+
+# object.method
+bugi.shape("turtle")
+bugi.color("blue")
+bugi.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight) #object.attrubute
+my_screen.exitonclick()
+
