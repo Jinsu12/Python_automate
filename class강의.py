90c5885a78d25449fb74043be9a8b0580ce54ee0
jinsu12 <jss910924@gmail.com>
2021-07-27 22:01:25 +0900

    DAY 17 (210727)

---
+++ class강의.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+
+
+user_1 = User("001", "Jinsu")
+
--- coffee.py
+++ coffee.py
@@ -29,3 +29,56 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+def enough_resources(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print("Sorry there is not enough water.")
+            return False
+        return True
+
+
+def transaction_successful(payment_done, drink_cost):
+    if payment_done >= drink_cost:
+        change = payment_done - drink_cost
+        print(f"Here is your change ${change}")
+        global profit
+        profit += drink_cost
+    else:
+        print(f"Sorry that's not enough money. ${payment_done} refunded.")
+
+
+def insert_coin():
+    print("Please insert coin")
+    total = int(input("How many quarters?")) * 0.25
+    total += int(input("How many dimes?")) * 0.10
+    total += int(input("How many nickels?")) * 0.05
+    total += int(input("How many pennies?")) * 0.01
+    return total
+
+
+def make_coffee(drink_ingredient):
+    for item in drink_ingredient:
+        resources[item] -= drink_ingredient[item]
+
+
+is_on = True
+profit = 0
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if enough_resources(drink['ingredients']):
+            payment = insert_coin()
+            transaction_successful(payment, drink['cost'])
+            make_coffee(drink['ingredients'])
+            print(f"Here is your {choice}, enjoy!")
